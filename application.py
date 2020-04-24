edb5c3ffe18d597a5f079e3ff0bf0e4f93573786
Josefine <josefineericsson@live.se>
2020-04-24 15:52:23 +0200

    db connection working starting to route pages and verify db info

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -10,15 +10,6 @@ import pprint
 import psycopg2
 
 app = Flask(__name__)
-"""
-Set environment for Flask in PS
-$env:FLASK_APP = "application.py"
-$env:FLASK_ENV = "development"
-$env:DATABASE_URL = "postgres://ggzvnyybarupid:7c8053cbd4563994056b27c2a076ed862ac94beb57fea449e03325e7f7f73709@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d5hbh8gkn06142"
-"""
-
-# Set database environment
-# os.environ["DATABASE_URL"] = secrets.DATABASE_URL
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -27,39 +18,51 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 Session(app)
+
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-db.execute("SELECT * FROM Users")
-db.commit()
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={
-    "key": secrets.key, "isbns": "9781632168146"})
-# pprint.pprint(res.json())
+table = db.execute("SELECT * FROM users")
+db.commit() 
 
+pprint.pprint(table)
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    if not session.get("logged_in"):
+        return render_template("welcome.html")
+    else:
+        return render_template("index.html", username=session["user_name"])
 
 @app.route("/registration", methods=["POST"])
 def reg():
     if request.method == "POST":
-        u_name = request.form.get("reg_username")
-        pw = request.form.get("reg_password")
-        email = request.form.get("reg_email")
-        #db.execute("INSERT INTO Users (username, password, email) VALUES (:name, :pw, :email)", 
-        #{"name":u_name, "pw":pw , "email":email})
-        #db.commit()
-        return render_template("reg.html")
+        users = db.execute("SELECT name, email FROM users")
+        for user in users:
+            if user.name == request.form.get("reg_username"):
+                return "Sorry username taken"
+            elif user.email == request.form.get("reg_email"):
+                return "Email already assign to an account"
+            else: 
+                u_name = request.form.get("reg_username")
+                pw = request.form.get("reg_password")
+                email = request.form.get("reg_email")
+                db.execute("INSERT INTO users (name, password, email) VALUES (:name, :pw, :email)", 
+                {"name":u_name, "pw":pw , "email":email})
+                db.commit()
+                return render_template("reg.html")
     else:
         return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        users = db.execute("SELECT name, password FROM users WHERE ")
+        return start()
+    else: 
+        return render_template("login.html")
 
 @app.route("/forgot")
 def forgot():
