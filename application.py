395137482b5fef2cc1d7d6b41128cb5d08db404d
Josefine <josefineericsson@live.se>
2020-04-30 14:43:25 +0200

    Styling and layout for forms and database logic

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 import os
-
-from flask import Flask, session, render_template, request, flash, redirect
+from flask import Flask, session, render_template, request, flash, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -10,8 +9,8 @@ import pprint
 import psycopg2
 from extra import RegistrationForm, LoginForm
 from flask_wtf import FlaskForm
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-from werkzeug import generate_password_hash
+from wtforms import BooleanField, StringField, PasswordField, validators
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -24,10 +23,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure CSRF 
+app.config['WTF_CSRF_ENABLED'] = False
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
-s = scoped_session(sessionmaker(bind=engine))
-db = s()
+db = scoped_session(sessionmaker(bind=engine))
+# db = s()
 
 # Session
 # session["logged_in"] = True/False
@@ -37,10 +40,8 @@ db = s()
 
 @app.route("/", methods=["GET"])
 def start():
-    print(request.form)
-    print(request.method)
     form = RegistrationForm(request.form)
-    if session.get("logged_user") is None:
+    if session.get("logged_in") is not True:
         # No stored session needs to log in
         return render_template("welcome.html", form=form)
     else:
@@ -49,44 +50,60 @@ def start():
 
 @app.route("/registration", methods=["GET", "POST"])
 def reg():
-    if request.method == "POST":
-        form = RegistrationForm(request.form)
-        if form.validate():
-            if not db.execute("SELECT name FROM users WHERE name=:name", {"name":form.name.data}):
-                return render_template("login_error.html", error_msg="Sorry username taken")
-            elif not db.execute("SELECT email FROM users WHERE email=:email", {"email":form.email.data}):
-                return render_template("login_error.html", error_msg="Email already assign to an account")
-            else: 
-                db.execute("INSERT INTO users (name, password, email) VALUES (:name, :pw, :email)", 
-                {"name":form.name.data, "pw":generate_password_hash(form.password.data),
-                "email":form.email.data})
-                db.commit()
-                flash("SUCCESS!")
-                return redirect(url_for('login'))
-        else:
-            return render_template("login_error.html", error_msg = "Form isn't validated, contact page-admin.")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if db.execute("SELECT name FROM users WHERE name=:name", {"name":form.name.data}).fetchone() is not None:
+            flash("The username is already in use")
+            return redirect(url_for('reg'))
+        elif db.execute("SELECT email FROM users WHERE email=:email", {"email":form.email.data}).fetchone() is not None:
+            flash("Email already assign to an account")
+            return redirect(url_for('reg'))
+        else: 
+            db.execute("INSERT INTO users (name, password, email) VALUES (:name, :pw, :email)", 
+            {"name":form.name.data, "pw":generate_password_hash(form.password.data),
+            "email":form.email.data})
+            db.commit()
+            flash("New account registred please log in to access BookReads.")
+            return redirect(url_for('login'))
     else: 
-        form = RegistrationForm(request.form)
         return render_template("welcome.html", form=form)
-
-
 
         
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    form=LoginForm(request.form)
-    if request.method == "POST" and form.validate:
-        users = db.execute("SELECT name, password FROM users WHERE name=:name", {"name":form.name.data})
-        if users is None:
+    form=LoginForm()
+    if form.validate_on_submit():
+        res = db.execute("SELECT password,id FROM users WHERE name=:name", {"name":form.name.data}).fetchone()
+        # check_pw = check_password_hash(res("password"), form.password.data)
+        # print(check_pw)
+        #user_id = res["id"]
+        #print(db_hash)
+        # print(users("name"))
+        if db.execute("SELECT name FROM users WHERE name=:name", {"name":form.name.data}).fetchone() is None:
+            flash("The username doesn't exsist")
             return redirect(url_for('login'))
-        #if check_password_hash(users.password): 
-            #session["logged_in"] = True
-           # session["user_id"] = users.id
-           #  session["user_name"] = users.name
-           # return redirect(url_for('start')
-    else: 
+        
+        # elif check_pw is False:
+        #     flash("Wrong password")
+        #     return redirect(url_for('login'))
+        else:
+            session["logged_in"] = True
+            #session["user_id"] = users.id
+            session["user_name"] = form.name.data
+            return redirect(url_for('start'))
+    else:
         return render_template("login.html", form=form)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("start"))
 
 @app.route("/forgot")
 def forgot():
     return render_template("forgot.html")
+
+
+          # hash_pw = check_password_hash(form.password.data)
+        # pw_db = generate_password_hash(form.password.data)
+        #if check_password_hash(users.password)
--- extra.py
+++ extra.py
@@ -1,33 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistrationForm(Form):
-    """Contact form."""
-    name = StringField('Username', [
-        validators.DataRequired(message="You need to provide a username")], 
+class RegistrationForm(FlaskForm):
+    """Registration form."""
+    name = StringField('Username', validators=[
+        DataRequired(message="You need to provide a username")], 
         id="reg_username")
     
-    email = StringField('Email Address', [
-        validators.Email(message=('Not a valid email address.')),
-        validators.DataRequired()], id="reg_email")
+    email = StringField('Email Address', validators=[
+        Email(message=('Not a valid email address.')),
+        DataRequired()], id="reg_email")
     
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match'), 
-        validators.Length(min=8, message=('Your password is too short.'))], id="reg_password")
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        EqualTo('confirm', message='Passwords must match'), 
+        Length(min=8, message=('Your password is too short.'))], id="reg_password")
     
-    confirm = PasswordField('Repeat Password', id="reg_password_confirm")
+    confirm = PasswordField('Repeat Password', validators=[DataRequired(message='Please confirm password')],id="reg_password_confirm")
     
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()], id="reg_agree")
+    submit = SubmitField('Submit')
+    # accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()], id="reg_agree")
 
-class LoginForm(Form):
-    """Contact form."""
-    name = StringField('Username', [
-        validators.DataRequired(message="You need to provide a username")], 
-        id="reg_username")
-    
+class LoginForm(FlaskForm):
+    """Login form."""
+    name = StringField('Username', validators=[
+        DataRequired(message="You need to provide a username")])
 
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        ], id="reg_password")
+    password = PasswordField('Password', validators=[
+        DataRequired(message="You need to provide a password")])
+
+    submit = SubmitField('Submit')
